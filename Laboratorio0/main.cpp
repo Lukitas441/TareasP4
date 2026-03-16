b4b4ecfd37fec27fc6e933220e6f8a16c83f277b
ikhasz <nohakeiko@gmail.com>
2026-03-15 21:00:28 -0300

    datatype DTExp y DTFecha

--- Laboratorio0/main.cpp
+++ Laboratorio0/main.cpp
@@ -3,8 +3,55 @@
 #include <map>
 
 // Datatypes
+class DTFecha
+{    
+    private:
+        int Dia;
+        int Mes;
+        int Año;
+    
+    public:
+        int getdia();
+        int getmes();
+        int getaño();
+};
 
+int DTFecha::getdia(){
+    return this->Dia;
+};
+int DTFecha::getmes(){
+    return this->Mes;
+};
+int DTFecha::getaño(){
+    return this->Año;
+};
 
+class DTExp
+{
+    private:
+        std::string codigoReserva;   
+        std::list<std::string> Turista;
+        std::string descripcion; 
+        DTFecha fecha;
+    
+    public:
+        std::string getCodigoReserva();
+        std::list<std::string> getTurista();
+        std::string getDescripcion();
+        DTFecha getFecha();
+};
+std::string DTExp::getCodigoReserva(){
+    return this->codigoReserva;
+};
+std::list<std::string> DTExp::getTurista(){
+    return this->Turista;
+};
+std::string DTExp::getDescripcion(){
+    return this->descripcion;
+};
+DTFecha DTExp::getFecha(){
+    return this->fecha;
+};
 
 // Clases
 
@@ -18,7 +65,7 @@ class Turista{
         Turista(std::string, std::string, std::string);     //constructor
         ~Turista();                          //destructor
         std::string toString();
-        std::string listarExperiencias(DTFecha, float, float);
+        std::list<std::string> listarExperiencias(DTFecha, float, float);
 };
 
 Turista::Turista(std::string ci, std::string nombre, std::string email){
@@ -34,7 +81,7 @@ std::string Turista::toString(){
     return this->ci + "->" + this->nombre + "/" + this->email;
 };
 
-std::string Turista::listarExperiencias(DTFecha desde, float min, float max){
+std::list<std::string> Turista::listarExperiencias(DTFecha desde, float min, float max){
     return 0
 };
 
