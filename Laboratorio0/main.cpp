42b6ebf33096882000e3d97bada84636c05a7b76
ikhasz <nohakeiko@gmail.com>
2026-03-18 19:38:13 -0300

    nada

--- Laboratorio0/main.cpp
+++ Laboratorio0/main.cpp
@@ -5,22 +5,18 @@
 #include "headers/Alojamiento.h"
 #include "headers/EventoCultural.h"
 #include "headers/TourGuiado.h"
+#include "headers/DTExp.h"
+#include "headers/DTFecha.h"
+#include "headers/Enumerados.h"
 
 
 
 // Aca en el main por lo que entendi tenenemos que 
-
 //Primero crear alojamiento:
-
-
-
-
 
 int main()
 {
-    return ;
-   
+    Alojamiento alojamiento1("reserva1", "Alojamiento en Hotel XYZ", 100, DTFecha(), "Hotel XYZ", allInclusive, 3);
+    alojamiento1.getDT();
+    return 0;
 }
-
-
-
