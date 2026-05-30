d5a67a90e8deabc57c86ef2e3d7da751c1e956e6
Lukitas441 <lucas.r.abimorad@gmail.com>
2026-05-30 16:17:16 -0300

    a

--- Laboratorio4/include/Usuario.h
+++ Laboratorio4/include/Usuario.h
@@ -13,18 +13,17 @@ protected:
     std::string nombre;
     std::string contrasena;
     std::string email;
-    std::map<std::string, Calificacion*> calificacionesRecibidas; 
+    std::map<std::string, Calificacion *> calificacionesRecibidas;
 
 public:
-    Usuario(std::string nickname, std::string nombre, std::string contrasena);
+    Usuario(std::string nickname, std::string nombre, std::string contrasena, std::string email);
     virtual ~Usuario();
 
     std::string getNickname();
     std::string getNombre();
     std::string getContrasena();
     std::string getEmail();
-    std::map<std::string, Calificacion*> getCalificacionesRecibidas();
-
+    std::map<std::string, Calificacion *> getCalificacionesRecibidas();
 
     DTUsuario getInfoUsuarios();
     float calificacionPromedio();
--- Laboratorio4/src/Usuario.cpp
+++ Laboratorio4/src/Usuario.cpp
@@ -1,56 +1,67 @@
 #include "../include/Usuario.h"
 
-Usuario::Usuario(std::string nickname, std::string nombre, std::string contrasena, std::string email) {
+Usuario::Usuario(std::string nickname, std::string nombre, std::string contrasena, std::string email)
+{
     this->nickname = nickname;
     this->nombre = nombre;
     this->contrasena = contrasena;
     this->email = email;
 
-    calificacionesRecibidas = std::map<std::string, Calificacion*>();
+    calificacionesRecibidas = std::map<std::string, Calificacion *>();
 }
 
 Usuario::~Usuario() {}
 
-std::string Usuario::getNickname(){
+std::string Usuario::getNickname()
+{
     return nickname;
 }
 
-DTUsuario Usuario::getInfoUsuarios() {
+DTUsuario Usuario::getInfoUsuarios()
+{
     return DTUsuario(nickname, nombre);
 }
 
-std::map<std::string, Calificacion*> Usuario::getCalificacionesRecibidas() {
+std::map<std::string, Calificacion *> Usuario::getCalificacionesRecibidas()
+{
     return calificacionesRecibidas;
 }
 
-float Usuario::calificacionPromedio(){
-    if (calificacionesRecibidas.empty()) {
+float Usuario::calificacionPromedio()
+{
+    if (calificacionesRecibidas.empty())
+    {
         return 0.0; // Si no hay calificaciones, el promedio es 0
     }
 
-    std::map<std::string, Calificacion*>::iterator it;
+    std::map<std::string, Calificacion *>::iterator it;
     int sumaPuntajes = 0;
-    for (it = calificacionesRecibidas.begin(); it != calificacionesRecibidas.end(); ++it) {
+    for (it = calificacionesRecibidas.begin(); it != calificacionesRecibidas.end(); ++it)
+    {
         sumaPuntajes += it->second->getPuntaje();
     }
 
     return sumaPuntajes / calificacionesRecibidas.size();
 }
 
-
 // chequear pq distinto del Diagrama d Comunicacion
 // esta va pal santi .l.
-bool Usuario::existeCalificado(std::string nicknameCalificador, int codigoViaje) {
-    if (calificacionesRecibidas.empty()){return false;}
+bool Usuario::existeCalificado(std::string nicknameCalificador, int codigoViaje)
+{
+    if (calificacionesRecibidas.empty())
+    {
+        return false;
+    }
 
-    std::map<std::string, Calificacion*>::iterator it;
-    for (it = calificacionesRecibidas.begin(); it != calificacionesRecibidas.end(); ++it) {
-       Usuario* usuarioCalificador = it->second->getUsuarioCalificador();
-       Viaje* viajeCalificado = it->second->getCaliViaje();
-       if (usuarioCalificador->getNickname() == nicknameCalificador && viajeCalificado->getCodigo() == codigoViaje) {
-           return true;
-           
-       }
+    std::map<std::string, Calificacion *>::iterator it;
+    for (it = calificacionesRecibidas.begin(); it != calificacionesRecibidas.end(); ++it)
+    {
+        Usuario *usuarioCalificador = it->second->getUsuarioCalificador();
+        Viaje *viajeCalificado = it->second->getCaliViaje();
+        if (usuarioCalificador->getNickname() == nicknameCalificador && viajeCalificado->getCodigo() == codigoViaje)
+        {
+            return true;
+        }
     }
     return false;
 }
